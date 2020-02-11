dd35e5562f148756bb1d9e679b25a436bd08f5fe
anshu0157 <38017870+anshu0157@users.noreply.github.com>
2020-02-11 23:34:13 +0530

    Update calculating min max prime reverse.py

--- calculating min max prime reverse.py
+++ calculating min max prime reverse.py
@@ -1,6 +1,6 @@
 from pythonfile import high,minm,prime,count_no,even,odd,sum_no,reverse
 def number(n):
-    print("highest number:",high(n))
+    print("highest number",high(n))
 
     print("minmum number:",minm(n))
 
@@ -15,7 +15,7 @@ def number(n):
     print("odd number:",end="")
     odd(n)
 
-    print("sum of the number:",sum_no(n))
+    print("sum of number:",sum_no(n))
     print("reverse of number:",end="")
     reverse(n)
     
